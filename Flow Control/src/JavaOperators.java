c825c60b2757422402c5fd7f95230975932b8f4c
Joey-Yannuzzi <joeygym2000@gmail.com>
2021-02-16 16:45:05 -0500

    HW-FlowControl and some notes

---
+++ Flow Control/src/JavaOperators.java
@@ -0,0 +1,21 @@
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Java Operators Exercise 1");
+		System.out.println(5 * 10);
+
+		System.out.println("Java Operators Exercise 2");
+		System.out.println(5 / 10);
+
+		System.out.println("Java Operators Exercise 3");
+		int x = 10;
+		++x;
+
+		System.out.println("Java Operators Exercise 4");
+		x += 5;
+	}
+
+}
